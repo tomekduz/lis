802940932252e08a9c2756ce27532e011956f2e0
Tomek Duziak <t.duziak@gmail.com>
2017-07-16 22:08:48 +0200

    add tslint

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ class Main {
 let sequenceNumber = [1, 9, 5, 13, 3, 11, 7, 15, 2, 10, 6, 14, 4, 12, 8, 16];
 let main = new Main();
 let result = main.run(sequenceNumber);
-console.log(result);
+
 /**
  * to show result in CLI use
  * console.log(result);
